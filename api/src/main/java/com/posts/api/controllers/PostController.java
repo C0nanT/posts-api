e29c4ed83496485ce96d8724373c99a7ce16efe2
Conan <conantorreswf@gmail.com>
2025-12-08 21:59:32 -0300

    fix: accept multipart/form-data body for creating posts with images

--- api/src/main/java/com/posts/api/controllers/PostController.java
+++ api/src/main/java/com/posts/api/controllers/PostController.java
@@ -2,23 +2,31 @@ package com.posts.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.posts.api.domains.post.PostRequestDTO;
 import com.posts.api.services.PostService;
 import com.posts.api.domains.post.Post;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/post")
 public class PostController {
-     
+
     @Autowired
     private PostService postService;
 
-    public ResponseEntity<Post> create(@RequestBody PostRequestDTO body) {
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<Post> create(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content,
+            @RequestParam("image") MultipartFile image,
+            @RequestParam("type_name") String typeName) {
+
+        PostRequestDTO body = new PostRequestDTO(title, content, image, typeName);
         Post newPost = this.postService.createPost(body);
         return ResponseEntity.ok(newPost);
     }
